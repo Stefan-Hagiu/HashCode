0a0689c53f94ff044398667c0df418ddc7b810f5
sxh1076 <sxh1076@cs.bham.ac.uk>
2018-03-01 19:16:49 +0000

    Na

--- src/InputReader.java
+++ src/InputReader.java
@@ -1,20 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class InputReader {
 
 	public static void read(String fileName) {
-		Scanner sc = new Scanner(fileName);
+		BufferedReader sc = new BufferedReader(new FileReader(fileName));
 
-		GlobalVariables.maxX = sc.nextInt();
-		GlobalVariables.maxY = sc.nextInt();
-		GlobalVariables.numberOfVehicles = sc.nextInt();
-		GlobalVariables.numberOfJobs = sc.nextInt();
-		GlobalVariables.bonus = sc.nextInt();
-		GlobalVariables.totalSteps = sc.nextInt();
+		GlobalVariables.maxX = sc.read()
+		GlobalVariables.maxY = sc.read();
+		GlobalVariables.numberOfVehicles = sc.read();
+		GlobalVariables.numberOfJobs = sc.read();
+		GlobalVariables.bonus = sc.read();
+		GlobalVariables.totalSteps = sc.read();
 
 		for (int i = 0; i < GlobalVariables.numberOfJobs; i++) {
-			GlobalVariables.listOfJobs.add(new JobDataStructure(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(),
-					sc.nextInt(), sc.nextInt(), i));
+			GlobalVariables.listOfJobs.add(new JobDataStructure(sc.read(), sc.read(), sc.read(), sc.read(),
+					sc.read(), sc.read(), i));
 		}
 	}
 
