d33ae5611790cdc9faf0f700d3d4e0781cddbec3
sxh1076 <sxh1076@cs.bham.ac.uk>
2018-03-01 19:30:46 +0000

    Ia paul

--- src/OutputWriter.java
+++ src/OutputWriter.java
@@ -5,6 +5,7 @@ public class OutputWriter {
 	public static PrintWriter out;
 
 	public static void write(ArrayList<JobDataStructure> data) {
+		out.write(data.size() + " ");
 		for (int i = 0; i < data.size(); i++) {
 			out.write(data.get(i).index + " ");
 		}
