c1facc49322f330be8b5069678a15ec3bad2fd63
sxh1076 <sxh1076@cs.bham.ac.uk>
2018-03-01 19:29:23 +0000

    Nu stiu

--- src/InputReader.java
+++ src/InputReader.java
@@ -1,25 +1,24 @@
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class InputReader {
 
 	public static void read(String fileName) {
-		BufferedReader sc;
 		try {
-			sc = new BufferedReader(new FileReader(fileName));
+			Scanner sc = new Scanner(new File(fileName));
 
-			GlobalVariables.maxX = sc.read();
-			GlobalVariables.maxY = sc.read();
-			GlobalVariables.numberOfVehicles = sc.read();
-			GlobalVariables.numberOfJobs = sc.read();
-			GlobalVariables.bonus = sc.read();
-			GlobalVariables.totalSteps = sc.read();
+			GlobalVariables.maxX = sc.nextInt();
+			GlobalVariables.maxY = sc.nextInt();
+			GlobalVariables.numberOfVehicles = sc.nextInt();
+			GlobalVariables.numberOfJobs = sc.nextInt();
+			GlobalVariables.bonus = sc.nextInt();
+			GlobalVariables.totalSteps = sc.nextInt();
 
 			for (int i = 0; i < GlobalVariables.numberOfJobs; i++) {
-				GlobalVariables.listOfJobs
-						.add(new JobDataStructure(sc.read(), sc.read(), sc.read(), sc.read(), sc.read(), sc.read(), i));
+				GlobalVariables.listOfJobs.add(new JobDataStructure(sc.nextInt(), sc.nextInt(), sc.nextInt(),
+						sc.nextInt(), sc.nextInt(), sc.nextInt(), i));
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
