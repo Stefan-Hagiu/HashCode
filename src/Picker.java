1f5300d42198fbe49ba319574b51f387d39933db
sxh1076 <sxh1076@cs.bham.ac.uk>
2018-03-01 18:58:51 +0000

    Repaired picker

--- src/Picker.java
+++ src/Picker.java
@@ -2,35 +2,34 @@ import java.util.ArrayList;
 
 public class Picker {
 
-  public static int best(Car car, ArrayList<JobDataStructure> rides) {
-    int max = -1;
-    int pick = -1;
-    for(rideIndex = 0; rideIndex < rides.size(); rideIndex++)
-    {
-      score = gain(car, rides[rideIndex]);
-      if(score > max)
-      {
-        max = score;
-        pick = rideIndex;
-      }
-    }
-    return pick;
-  }
+	public static int best(Car car, ArrayList<JobDataStructure> rides) {
+		double max = -1;
+		int pick = -1;
+		double score = 0;
+		for (int rideIndex = 0; rideIndex < rides.size(); rideIndex++) {
+			score = gain(car, GlobalVariables.listOfJobs.get(rideIndex));
+			if (score > max) {
+				max = score;
+				pick = rideIndex;
+			}
+		}
+		return pick;
+	}
 
-  gain(Car car, JobDataStructure ride)
-  {
-    int arrivalT = car.time + Math.abs(car.posx - ride.startingX) + Math.abs(car.posy - ride.startingY);
-    destinationT = Math.max(arrivalT, ride.earliestStart) + ride.totalDistance;
-    if(destinationT > ride.latestFinish)
-    {
-      return 0;
-    }
-    if(arrivalT <= ride.earliestStart)
-    {
-      score = GlobalVariables.bonus;
-    }
-    score = score + ride.totalDistance;
-    return score / (destinationT - car.time);
-  }
+	public static double gain(Car car, JobDataStructure ride) {
+		int arrivalT, destinationT;
+		int score = 0;
+
+		arrivalT = car.time + Math.abs(car.posx - ride.startingX) + Math.abs(car.posy - ride.startingY);
+		destinationT = Math.max(arrivalT, ride.earliestStart) + ride.totalDistance;
+		if (destinationT > ride.latestFinish) {
+			return 0;
+		}
+		if (arrivalT <= ride.earliestStart) {
+			score = GlobalVariables.bonus;
+		}
+		score = score + ride.totalDistance;
+		return score / (destinationT - car.time);
+	}
 
 }
