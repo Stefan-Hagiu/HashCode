597cfdc115d4a0b83a9a55e7a68bef2abe3edda4
sxh1076 <sxh1076@cs.bham.ac.uk>
2018-03-01 19:18:11 +0000

    FGOGOGOOGOGO

--- src/InputReader.java
+++ src/InputReader.java
@@ -1,23 +1,34 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class InputReader {
 
 	public static void read(String fileName) {
-		BufferedReader sc = new BufferedReader(new FileReader(fileName));
+		BufferedReader sc;
+		try {
+			sc = new BufferedReader(new FileReader(fileName));
 
-		GlobalVariables.maxX = sc.read()
-		GlobalVariables.maxY = sc.read();
-		GlobalVariables.numberOfVehicles = sc.read();
-		GlobalVariables.numberOfJobs = sc.read();
-		GlobalVariables.bonus = sc.read();
-		GlobalVariables.totalSteps = sc.read();
+			GlobalVariables.maxX = sc.read();
+			GlobalVariables.maxY = sc.read();
+			GlobalVariables.numberOfVehicles = sc.read();
+			GlobalVariables.numberOfJobs = sc.read();
+			GlobalVariables.bonus = sc.read();
+			GlobalVariables.totalSteps = sc.read();
 
-		for (int i = 0; i < GlobalVariables.numberOfJobs; i++) {
-			GlobalVariables.listOfJobs.add(new JobDataStructure(sc.read(), sc.read(), sc.read(), sc.read(),
-					sc.read(), sc.read(), i));
+			for (int i = 0; i < GlobalVariables.numberOfJobs; i++) {
+				GlobalVariables.listOfJobs
+						.add(new JobDataStructure(sc.read(), sc.read(), sc.read(), sc.read(), sc.read(), sc.read(), i));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
 	}
 
 }
