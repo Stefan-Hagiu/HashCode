d1017aa931b124d029ae35c7e46cb093b90c76cd
sxh1076 <sxh1076@cs.bham.ac.uk>
2018-03-01 18:45:36 +0000

    Fixed Car

--- src/Car.java
+++ src/Car.java
@@ -1,29 +1,27 @@
+import java.util.ArrayList;
+
 public class Car {
 
-  public int intex;
-  public int time;
-  public int posx;
-  public int posy;
-  public ArrayList<JobDataStructure> orders;
+	public int index;
+	public int time;
+	public int posx;
+	public int posy;
+	public ArrayList<JobDataStructure> orders;
 
-	public Car(_index) {
-    this.index = _index;
+	public Car(int _index) {
+		this.index = _index;
 		this.posx = 0;
 		this.posy = 0;
 		this.time = 0;
 	}
 
-  public schedule(JobDataStructure r) {
-    int arrivalT = this.time + Math.abs(this.posx - r.startingX) + Math.abs(this.posy - r.startingY);
-    int destinationT = Math.max(arrivalT, r.earliestStart) + r.totalDistance;
-    this.posx = r.finishX;
-    this.posy = r.finishY;
-    this.time = destinationT;
-    orders.add(r);
-  }
-
-  public printOrders() {
-    // orders???
-  }
+	public void schedule(JobDataStructure r) {
+		int arrivalT = this.time + Math.abs(this.posx - r.startingX) + Math.abs(this.posy - r.startingY);
+		int destinationT = Math.max(arrivalT, r.earliestStart) + r.totalDistance;
+		this.posx = r.finishX;
+		this.posy = r.finishY;
+		this.time = destinationT;
+		orders.add(r);
+	}
 
 }
