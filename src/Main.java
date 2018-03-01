acc199812a2361074b828c0bc6c296a3692d1933
iarba <dociarba@gmail.com>
2018-03-01 20:02:26 +0100

    added main loop

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,20 @@ public class Main {
 		}
 		InputReader.read(args[0]);
 
+    for(i = 0; i < GlobalVariables.numberOfVehicles; i++) {
+      Car car = new Car(i);
+      while(true){
+        int pick = Picker.best(car, GlobalVariables.listOfJobs);
+        if(pick == -1) {
+          break;
+        } else {
+          car.schedule(GlobalVariables.listOfJobs[pick])
+          GlobalVariables.listOfJobs.remove(pick);
+        }
+      }
+      // print car :)
+    }
+
 		OutputWriter.write(args[1]);
 	}
 
